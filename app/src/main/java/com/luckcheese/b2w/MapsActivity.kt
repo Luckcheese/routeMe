753c0064ec5012d1d7fa3b5a382d95330c72d602
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2020-12-09 12:08:37 -0300

    Add search for Places
    - Add google places lib
    - Add AutocompleteSupportFragment's search bar to MapsActivity layout
    - Init AutocompleteSupportFragment to search places and returns it's name, location and address

--- app/src/main/java/com/luckcheese/b2w/MapsActivity.kt
+++ app/src/main/java/com/luckcheese/b2w/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.luckcheese.b2w
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.api.Status
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,8 +10,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, PlaceSelectionListener {
 
     private lateinit var mMap: GoogleMap
 
@@ -19,12 +24,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
 
         setupMaps()
+        setupSearch()
     }
 
     private fun setupMaps() {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    private fun setupSearch() {
+        Places.initialize(applicationContext, getString(R.string.google_maps_key))
+
+        val autocompleteFragment = supportFragmentManager
+            .findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        autocompleteFragment.setPlaceFields(
+            listOf(
+                Place.Field.ID,
+                Place.Field.NAME,
+                Place.Field.LAT_LNG,
+                Place.Field.ADDRESS
+            )
+        )
+        autocompleteFragment.setOnPlaceSelectedListener(this)
     }
 
     // ----- OnMapReadyCallback -----
@@ -36,5 +58,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    // ----- PlaceSelectionListener -----
+
+    override fun onPlaceSelected(place: Place) {
+
+    }
+
+    override fun onError(status: Status) {
+        if (status != Status.RESULT_CANCELED) {
+            // TODO:show error
+        }
     }
 }
